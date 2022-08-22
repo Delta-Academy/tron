8bbc43d2f857f4cbbd72878ea4cf544265813ede
neuromantic99 <jrowlandm@gmail.com>
2022-08-22 13:14:58 +0100

    added existing snake code

--- delta_snake/main.py
+++ delta_snake/main.py
@@ -1,22 +1,27 @@
 from typing import Any, Dict
 
+import numpy as np
+
 from check_submission import check_submission
-from game_mechanics import Env, choose_move_randomly, load_pkl, play_game, save_pkl
+from game_mechanics import Snake, choose_move_randomly, load_network, play_snake, save_network
+
+from torch import nn
 
 TEAM_NAME = "Team Name"  # <---- Enter your team name here!
 assert TEAM_NAME != "Team Name", "Please change your TEAM_NAME!"
 
 
-def train() -> Dict:
+def train() -> nn.Module:
     """
     TODO: Write this function to train your algorithm.
 
     Returns:
     """
+
     raise NotImplementedError("You need to implement this function!")
 
 
-def choose_move(state: Any, user_file: Any, verbose: bool = False) -> int:
+def choose_move(state: np.ndarray, network: Any) -> int:
     """Called during competitive play. It acts greedily given current state of the board and value
     function dictionary. It returns a single move to play.
 
@@ -31,27 +36,27 @@ def choose_move(state: Any, user_file: Any, verbose: bool = False) -> int:
 if __name__ == "__main__":
 
     ## Example workflow, feel free to edit this! ###
-    file = train()
-    save_pkl(file, TEAM_NAME)
+    network = train()
+    save_network(network, TEAM_NAME)
 
     check_submission(
         TEAM_NAME
     )  # <---- Make sure I pass! Or your solution will not work in the tournament!!
 
-    my_value_fn = load_pkl(TEAM_NAME)
+    my_value_fn = load_network(TEAM_NAME)
 
     # Code below plays a single game against a random
     #  opponent, think about how you might want to adapt this to
     #  test the performance of your algorithm.
-    def choose_move_no_value_fn(state: Any) -> int:
+    def choose_move_no_network(state: Any) -> int:
         """The arguments in play_game() require functions that only take the state as input.
 
         This converts choose_move() to that format.
         """
         return choose_move(state, my_value_fn)
 
-    play_game(
-        your_choose_move=choose_move_no_value_fn,
+    play_snake(
+        your_choose_move=choose_move_no_network,
         opponent_choose_move=choose_move_randomly,
         game_speed_multiplier=1,
         render=True,
