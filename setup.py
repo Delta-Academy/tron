cd8ec07f19ce2b0b129911ad769fda90e4a335d3
James Rowland <jrowlandm@gmail.com>
2022-09-19 09:52:09 +0100

    Update setup.py
    Co-authored-by: Henry Pulver <henrypulver@hotmail.co.uk>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="delta_snake",
     version="0.1",
     packages=["delta_snake"],
-    description="tron / snake with an openAi gym stule interface",
+    description="tron / snake with an openAI gym style interface",
     author="neuromantic99",
     author_email="james@learney.me",
 )
